cc073e2eee8b50ab208da499dbdfb7f8f7274185
tochman <thomas@nocebo.se>
2015-10-09 23:58:36 +0200

    refactored update controller

--- www/js/updateController.js
+++ www/js/updateController.js
@@ -1,25 +1,25 @@
 foodRatingApp.controller("UpdateController", function($scope) {
     var deploy = new Ionic.Deploy();
-  
+
     // Update app code with new release from Ionic Deploy
     $scope.doUpdate = function() {
       deploy.update().then(function(res) {
-        console.log('Ionic Deploy: Update Success! ', res);
+        $scope.log = 'Ionic Deploy: Update Success! ' + res;
       }, function(err) {
-        console.log('Ionic Deploy: Update error! ', err);
+        $scope.log = 'Ionic Deploy: Update error! ' + err;
       }, function(prog) {
-        console.log('Ionic Deploy: Progress... ', prog);
+        $scope.log = 'Ionic Deploy: Progress... ' + prog;
       });
     };
 
     // Check Ionic Deploy for new code
     $scope.checkForUpdates = function() {
-      console.log('Ionic Deploy: Checking for updates');
+      $scope.log = 'Ionic Deploy: Checking for updates';
       deploy.check().then(function(hasUpdate) {
-        console.log('Ionic Deploy: Update available: ' + hasUpdate);
+        $scope.log ='Ionic Deploy: Update available: ' + hasUpdate;
         $scope.hasUpdate = hasUpdate;
       }, function(err) {
-        console.error('Ionic Deploy: Unable to check for updates', err);
+        $scope.log = 'Ionic Deploy: Unable to check for updates' + err;
       });
-    }	
+    }
 });
