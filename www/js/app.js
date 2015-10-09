19eeaf3e7108fd893fc6c46bb4ee371c39f7e1fe
tochman <thomas@nocebo.se>
2015-10-09 23:39:20 +0200

    added deploy and analytics

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var foodRatingApp = angular.module('foodRatingApp', ['ionic', 'ionic.service.core', 'ngCordova']);
+var foodRatingApp = angular.module('foodRatingApp', ['ionic','ionic.service.core', 'ionic.service.analytics', 'ngCordova']);
 
 foodRatingApp.config(function($ionicConfigProvider) {
   $ionicConfigProvider.tabs.position('bottom');
 })
 
-  .run(function ($ionicPlatform) {
+  .run(function ($ionicPlatform, $ionicAnalytics) {
 
     $ionicPlatform.ready(function () {
+      $ionicAnalytics.register();
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins.Keyboard) {
