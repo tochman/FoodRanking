2b4235d9d4feadec0eded94faf3e465b29f47d54
tochman <thomas@nocebo.se>
2015-10-12 07:53:53 +0200

    refactorings. added README!

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+######Invite/share application
+```
+ionic share <email>
+```
+
+######Deploying using CLI
+```
+ionic upload --note "new version..." --deploy=production
+```
+
+
--- www/js/updateController.js
+++ www/js/updateController.js
@@ -1,25 +1,44 @@
-foodRatingApp.controller("UpdateController", function($scope) {
-    var deploy = new Ionic.Deploy();
+foodRatingApp.controller("UpdateController", function ($scope, $ionicLoading) {
+  var deploy = new Ionic.Deploy();
 
-    // Update app code with new release from Ionic Deploy
-    $scope.doUpdate = function() {
-      deploy.update().then(function(res) {
-        $scope.log = 'Ionic Deploy: Update Success! ' + res;
-      }, function(err) {
-        $scope.log = 'Ionic Deploy: Update error! ' + err;
-      }, function(prog) {
-        $scope.log = 'Ionic Deploy: Progress... ' + prog;
-      });
-    };
+  // Update app code with new release from Ionic Deploy
+  $scope.doUpdate = function () {
 
-    // Check Ionic Deploy for new code
-    $scope.checkForUpdates = function() {
-      $scope.log = 'Ionic Deploy: Checking for updates';
-      deploy.check().then(function(hasUpdate) {
-        $scope.log = 'Ionic Deploy: Update available.';
-        $scope.hasUpdate = hasUpdate;
-      }, function(err) {
-        $scope.log = 'Ionic Deploy: Unable to check for updates' + err;
+    deploy.download().then(function () {
+      $scope.show('Downloading update');
+      deploy.extract().then(function () {
+        deploy.load();
+      }, function (error) {
+        $scope.show('Error installing! ' + error);
+      }, function (progress) {
+        $scope.log = 'Installing ' + progress;
       });
-    }
+    }, function (error) {
+      $scope.show('Error downloading! ' + error);
+    }, function (progress) {
+      $scope.show('Downloading.. ' + progress);
+    });
+
+  };
+
+  // Check Ionic Deploy for new code
+  $scope.checkForUpdates = function () {
+    $scope.show('Checking for updates');
+    deploy.check().then(function (hasUpdate) {
+      $scope.show('Ionic Deploy: Update available.');
+      $scope.hasUpdate = hasUpdate;
+    }, function (err) {
+      $scope.show('Unable to check for updates' + err);
+    });
+  };
+
+
+  $scope.show = function (message) {
+    $ionicLoading.show({
+      template: message
+    });
+  };
+  $scope.hide = function () {
+    $ionicLoading.hide();
+  };
 });
